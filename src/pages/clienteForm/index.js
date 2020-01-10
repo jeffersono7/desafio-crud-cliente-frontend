809d2f9b10666217c6cd050edc588b56c2826366
Jefferson Farias <jefferson.farias7@gmail.com>
2020-01-10 14:53:40 -0300

    criação do form'

--- src/pages/clienteForm/index.js
+++ src/pages/clienteForm/index.js
@@ -1,57 +1,141 @@
 import React from 'react';
-
+import * as Yup from 'yup';
 import clienteApi from '../../services/cliente';
 import tipoTelefoneApi from '../../services/tipoTelefone';
 
 import './styles.css';
+import { withFormik, Form, Field, ErrorMessage } from 'formik';
+import Header from '../../components/header';
+
+
+const schema = Yup.object().shape({
+    nome: Yup.string()
+        .required('Campo obrigatório')
+        .min(3)
+        .max(100),
+    cpf: Yup.string()
+        .required('Campo obrigatório')
+        .min(11, 'Campo obrigatório')
+        .max(11, 'Campo obrigatório'),
+    cep: Yup.string()
+        .required('Campo obrigatório')
+        .min(8, 'Campo obrigatório')
+        .max(8, 'Campo obrigatório'),
+    logradouro: Yup.string()
+        .required('Campo obrigatório'),
+    bairro: Yup.string()
+        .required('Campo obrigatório'),
+    cidade: Yup.string()
+        .required('Campo obrigatório'),
+    uf: Yup.string()
+        .required('Campo obrigatório')
+        .min(2)
+        .max(2),
+    complemento: Yup.string()
+});
+
+const handleSubmit = (values) => {
+    const cliente = {};
+
+    console.log(values);
+}
+
+const enhanceWithFormik = withFormik({
+    mapPropsToValues: () => ({ nome: '', cpf: '' }),
+    handleSubmit: handleSubmit,
+    validateOnChange: true,
+    validationSchema: schema
+})
+
+const form = props => {
+
+    return (
+        <div className="cliente-container">
+
+            <Form>
+                <div>
+                    <Field name="nome" placeholder="Nome" />
+                    <br />
+                    <ErrorMessage name="nome" />
+                </div>
+
+                <div>
+                    <Field name="cpf" placeholder="CPF" />
+                    <br />
+                    <ErrorMessage name="cpf" />
+                </div>
+
+                <div>
+                    <Field name="cep" placeholder="CEP" />
+                    <br />
+                    <ErrorMessage name="cep" />
+                </div>
+
+                <div>
+                    <Field name="logradouro" placeholder="Logradouro" />
+                    <br />
+                    <ErrorMessage name="logradouro" />
+                </div>
+
+                <div>
+                    <Field name="bairro" placeholder="Bairro" />
+                    <br />
+                    <ErrorMessage name="bairro" />
+                </div>
+
+                <div>
+                    <Field name="cidade" placeholder="Cidade" />
+                    <br />
+                    <ErrorMessage name="cidade" />
+                </div>
+
+                <div>
+                    <Field name="uf" placeholder="UF" />
+                    <br />
+                    <ErrorMessage name="uf" />
+                </div>
+
+                <div>
+                    <Field name="complemento" placeholder="Complemento" />
+                    <br />
+                    <ErrorMessage name="complemento" />
+                </div>
+
+                <button type="submit">Enviar</button>
+            </Form>
+        </div>
+    )
+}
+
+const MyForm = enhanceWithFormik(form);
 
 export default class ClienteForm extends React.Component {
 
     state = {
-        nome: '',
-        cpf: '',
-        tiposTelefone: []
+        tiposTelefone: [],
     }
 
     async componentDidMount() {
+
         const response = await tipoTelefoneApi.obterTodos();
 
-        this.setState({ tipoTelefone: response.data });
-    }
-
-    handleSubmit = (values, { props }) => {
-        const { id } = props.match.params;
-
-        const cliente = {};
-
-        console.log(values);
+        this.setState({ tiposTelefone: response.data });
     }
 
     render() {
         return (
-            <div className="cliente-container">
+            <div className="cliente-form">
+                {/* <Header /> */}
 
-                <form onSubmit={this.handleSubmit}>
+                <div className="titulo">
+                    <h2>Cadastro de cliente</h2>
+                </div>
 
-                    <input
-                        placeholder="Nome"
-                        type="text"
-                        name="nome"
-                        onChange={e => this.setState({ nome: e.target.value })}
-                        value={this.state.nome}
-                    />
+                <hr />
 
-                    <input
-                        placeholder="CPF"
-                        type="text"
-                        name="cpf"
-                        onChange={e => this.setState({ cpf: e.target.value })}
-                        value={this.state.cpf}
-                    />
-
-                </form>
-
+                <MyForm tiposTelefone={this.state.tiposTelefone} />
             </div>
-        );
+        )
     }
 }
+
